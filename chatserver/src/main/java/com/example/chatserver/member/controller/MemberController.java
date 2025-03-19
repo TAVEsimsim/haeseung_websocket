a38904bc5fabb6891111909ec7f0779ac4fb664b
GOOHAESEUNG <amu6675@naver.com>
2025-03-20 01:00:00 +0900

    fix : 비밀번호 암호화

--- chatserver/src/main/java/com/example/chatserver/member/controller/MemberController.java
+++ chatserver/src/main/java/com/example/chatserver/member/controller/MemberController.java
@@ -1,6 +1,7 @@
 package com.example.chatserver.member.controller;
 
 import com.example.chatserver.member.domain.Member;
+import com.example.chatserver.member.dto.MemberLoginReqDto;
 import com.example.chatserver.member.dto.MemberSaveReqDto;
 import com.example.chatserver.member.service.MemberService;
 import org.springframework.http.HttpStatus;
@@ -23,5 +24,13 @@ public class MemberController {
     public ResponseEntity<?> memberCreate(@RequestBody MemberSaveReqDto memberSaveReqDto) {
         Member member = memberService.create(memberSaveReqDto);
         return new ResponseEntity<>(member.getId(), HttpStatus.CREATED);
+    }
+
+    @PostMapping("/doLogin")
+    public ResponseEntity<?> doLogin(@RequestBody MemberLoginReqDto memberLoginReqDto) {
+        //email. pw 검증
+       Member member =  memberService.login(memberLoginReqDto);
+
+        //일치할 경우 access 토큰 발행
     }
 }
