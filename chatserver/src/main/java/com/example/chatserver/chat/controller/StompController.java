398174ab7b2d3936117a7f98be8305a25d712a12
GOOHAESEUNG <amu6675@naver.com>
2025-04-02 20:22:48 +0900

    feat : 채팅 메시지 저장

--- chatserver/src/main/java/com/example/chatserver/chat/controller/StompController.java
+++ chatserver/src/main/java/com/example/chatserver/chat/controller/StompController.java
@@ -1,6 +1,7 @@
 package com.example.chatserver.chat.controller;
 
 import com.example.chatserver.chat.dto.ChatMessageReqDto;
+import com.example.chatserver.chat.service.ChatService;
 import org.springframework.messaging.handler.annotation.DestinationVariable;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.SendTo;
@@ -10,24 +11,18 @@ import org.springframework.stereotype.Controller;
 @Controller
 public class StompController {
     private final SimpMessagingTemplate messageTemplate;
+    private final ChatService chatService;
 
-    public StompController(SimpMessagingTemplate messageTemplate) {
+    public StompController(SimpMessagingTemplate messageTemplate, ChatService chatService) {
         this.messageTemplate = messageTemplate;
+        this.chatService = chatService;
     }
-//    // 방법 1. MessageMapping(수신)과 SendTo 한번에 처리
-//    @MessageMapping("/{roomId}") //클라이언트에서 특정 publish/roomId 형태로 메시지 발행시 MessageMapping 수신
-//    @SendTo("/topic/{roomId}") //해당 roomId에 메시지 발행하여 구독중인 클라이언트에게 전송
-//    //DestinationVariable : @MessageMapping 어노테이션으로 정의된 websocket controller 내에서만 사용
-//    public String sendMessage(@DestinationVariable Long roomId, String message) {
-//        System.out.println(message);
-//
-//        return message;
-//    }
 
     //방법 2. MessageMapping어노테이션만 활용
     @MessageMapping("/{roomId}")
     public void sendMessage(@DestinationVariable Long roomId, ChatMessageReqDto chatMessageReqDto) {
         System.out.println(chatMessageReqDto.getMessage());
+        chatService.saveMessage(roomId, chatMessageReqDto);
         messageTemplate.convertAndSend("/topic/"+roomId, chatMessageReqDto);
     }
 }
--- chatserver/src/main/java/com/example/chatserver/chat/service/ChatService.java
+++ chatserver/src/main/java/com/example/chatserver/chat/service/ChatService.java
@@ -1,12 +1,22 @@
 package com.example.chatserver.chat.service;
 
+import com.example.chatserver.chat.domain.ChatMessage;
+import com.example.chatserver.chat.domain.ChatParticipant;
+import com.example.chatserver.chat.domain.ChatRoom;
+import com.example.chatserver.chat.domain.ReadStatus;
+import com.example.chatserver.chat.dto.ChatMessageReqDto;
 import com.example.chatserver.chat.repository.ChatMessageRepository;
 import com.example.chatserver.chat.repository.ChatParticipantRepository;
 import com.example.chatserver.chat.repository.ChatRoomRepository;
 import com.example.chatserver.chat.repository.ReadStatusRepository;
+import com.example.chatserver.member.domain.Member;
 import com.example.chatserver.member.repository.MemberRepository;
+import jakarta.persistence.EntityNotFoundException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 @Transactional
@@ -25,5 +35,33 @@ public class ChatService {
         this.chatParticipantRepository = chatParticipantRepository;
         this.readStatusRepository = readStatusRepository;
         this.memberRepository = memberRepository;
+    }
+
+    public void saveMessage(Long roomId, ChatMessageReqDto chatMessageReqDto) {
+//        채팅방 조회
+        ChatRoom chatRoom = chatRoomRepository.findById(roomId).orElseThrow(() -> new EntityNotFoundException("room cannot be found"));
+
+//        보낸 사람 조회
+        Member sender = memberRepository.findByEmail(chatMessageReqDto.getSenderEmail()).orElseThrow(() -> new EntityNotFoundException("member cannot be found"));
+
+//        메시지 저장
+        ChatMessage chatMessage = ChatMessage.builder()
+                .chatRoom(chatRoom)
+                .member(sender)
+                .content(chatMessageReqDto.getMessage())
+                .build();
+        chatMessageRepository.save(chatMessage);
+
+//        사용자별로 읽음 여부 저장
+        List<ChatParticipant> chatParticipants = chatParticipantRepository.findByChatRoom(chatRoom);
+        for(ChatParticipant c : chatParticipants) {
+            ReadStatus readStatus = ReadStatus.builder()
+                    .chatRoom(chatRoom)
+                    .member(c.getMember())
+                    .chatMessage(chatMessage)
+                    .isRead(c.getMember().equals(sender))
+                    .build();
+            readStatusRepository.save(readStatus);
+        }
     }
 }
